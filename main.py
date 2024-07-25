cda9e5968d5c31b14809d57f6956ab479df08a0e
Mihailo Škorić <procesaur@gmail.com>
2024-07-25 16:50:40 +0200

    init

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, render_template, Response
+from os import environ, path as px
+from rq_handler import process_req
+from base64 import b64encode
+
+
+app = Flask(__name__)
+app.config["DEBUG"] = False
+
+
+@app.route('/')
+def home():
+    return render_template('ui.html')
+
+@app.route('/help')
+def about():
+    return render_template('help.html', cover=px.join('static', 'cover.png'))
+
+@app.route('/img')
+def img():
+    return render_template('img.html', cover=px.join('static', 'cover.png'))
+
+@app.route('/process', methods=['POST', 'GET'])
+def api():
+    return "OK"
+
+if __name__ == "__main__":
+    port = int(environ.get("PORT", 5001))
+    app.run(host='0.0.0.0', port=port)
