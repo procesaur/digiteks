47ee09829352ec24f98f462a41588e8a513cfbf1
procesaur <31268468+procesaur@users.noreply.github.com>
2024-12-16 16:29:50 +0100

    js za editovanje

--- static/hocr.js
+++ static/hocr.js
@@ -3,8 +3,22 @@ function editable(top=0.94, saturation=0.5) {
 
     for ( i = 0; i < words.length; i++ ) {
         var conf = parseInt(words[i].title.substring(words[i].title.lastIndexOf(' ')))/100;
-        words[i].style  = "--conf:"+conf;
+        words[i].style  = "--red:255; --conf:"+conf;
+        words[i].dataset.original = words[i].innerText;
         words[i].contentEditable = 'true';
+        words[i].addEventListener('blur', handleTextChange);
     }  
 }
+
+function handleTextChange(event) {
+    const oldValue = event.target.dataset.original;
+    const newValue = event.target.innerText;
+    if (oldValue != newValue){
+        event.target.style  = "--red:0; --conf:0";
+    }
+    else{
+        event.target.style  = "--red:255; --conf:1";
+    }
+}
+
 window.onload = editable;
